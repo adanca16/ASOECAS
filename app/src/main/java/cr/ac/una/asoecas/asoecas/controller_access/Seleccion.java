184cf5ae8f5f155d0723db5afa32d566093372f9
adanca16 <adanca16@gmail.com>
2018-05-17 18:37:54 -0600

    Subiendo el documento modulado

--- app/src/main/java/cr/ac/una/asoecas/asoecas/controller_access/Seleccion.java
+++ app/src/main/java/cr/ac/una/asoecas/asoecas/controller_access/Seleccion.java
@@ -1,26 +1,30 @@
-package cr.ac.una.asoecas.asoecas;
+package cr.ac.una.asoecas.asoecas.controller_access;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.view.GravityCompat;
-import android.support.v4.widget.DrawerLayout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
 import android.widget.Button;
-import android.widget.Toast;
 // Activity.getFragmentManager
 
-import android.support.v4.app.FragmentActivity;
-import android.support.v4.app.FragmentManager;
+import cr.ac.una.asoecas.asoecas.Upload_image;
+import cr.ac.una.asoecas.asoecas.controller.Home_user;
+import cr.ac.una.asoecas.asoecas.controller_access.Login;
+import cr.ac.una.asoecas.asoecas.controller_access.Register;
+import cr.ac.una.asoecas.asoecas.model.FragmentoAbsPrincipal;
+import cr.ac.una.asoecas.asoecas.model.FragmentoActividadAbsPrincipal;
+import cr.ac.una.asoecas.asoecas.R;
+import cr.ac.una.asoecas.asoecas.model.Usuario_Datos;
 
 
 public class Seleccion extends FragmentoAbsPrincipal implements View.OnClickListener {
     Button accederGuest;
     Button accederLogin;
     Button registro;
+
+    WebView webViewSeleccion;
 
     public Seleccion() {
         // Required empty public constructor
@@ -29,7 +33,7 @@ public class Seleccion extends FragmentoAbsPrincipal implements View.OnClickList
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        getActivity().setTitle("ACCESO ASOECAS");
     }
 
 
@@ -44,23 +48,31 @@ public class Seleccion extends FragmentoAbsPrincipal implements View.OnClickList
         accederGuest.setOnClickListener(this);
         accederLogin.setOnClickListener(this);
         registro.setOnClickListener(this);
+/*
+        webViewSeleccion = (WebView) vista.findViewById(R.id.webViewSeleccion);
+        webViewSeleccion.getSettings().setJavaScriptEnabled(true);
+        webViewSeleccion.getSettings().setBuiltInZoomControls(true);
+        webViewSeleccion.loadUrl("https://asoecas.000webhostapp.com/carrusel_app.html");
+*/
+
         return vista;
     }
 
     @Override
     public void onClick(View v) {
         if (v.getId() == R.id.btnRegister_main){
-            ((FragmentoActividadAbsPrincipal) getActivity()).agregueFragmentoAPila(new Register());
+            //((FragmentoActividadAbsPrincipal) getActivity()).agregueFragmentoAPila(new Register());
+            ((FragmentoActividadAbsPrincipal) getActivity()).agregueFragmentoAPila(new Upload_image());
         }
         if (v.getId() == R.id.btnInvitado_main){
             Usuario_Datos.id= "invitado";
             Usuario_Datos.nombre = "Invitado";
-            Usuario_Datos.apellido = "Guest";
+            Usuario_Datos.apellido = "UNA";
             Usuario_Datos.carrera = "Null";
             Usuario_Datos.correo = "una@una.una";
             Usuario_Datos.telefono = "2764-0234";
             //Usuario_Datos.imagen = "https://asoecas.000webhostapp.com/picture/user/icono_default.png";
-            Usuario_Datos.imagen = "http://127.0.0.1:81/Api-android/picture/user/icono_default.png";
+            Usuario_Datos.imagen = "https://asoecas.000webhostapp.com/picture/user/icono_default.png";
 
             Usuario_Datos.rol = 4;
 
